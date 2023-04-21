5582b07763d0e8563d1341eafab818b55d278fad
Farhan <mansurifarhanfm@gmail.com>
2023-04-21 18:32:39 +0530

    added #2

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,29 @@
+import AnimatedBody from '@/components/AnimatedText';
+import AnimatedTitle from '@/components/AnimatedTitle';
 import TechStack from '@/components/TechStack'
-
-import React from 'react'
+import { gsap } from 'gsap';
+import React, { useEffect, useRef } from 'react'
 
 
 export default function About() {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.fromTo(
+        ref.current,
+        { display: "none", translateY: 100 },
+        {
+          display: "flex",
+          translateY: 0,
+          stagger: 0.1,
+          duration: 1,
+          ease: "power2.out",
+        }
+      );
+    }, ref);
 
+    return () => ctx.revert();
+  }, []);
 
   return (
 
@@ -17,24 +36,26 @@ export default function About() {
         <div className='w-full lg:border border-neutral-500/20  justify-center p-4 flex flex-col gap-4'>
 
           <div className='text-lg w-full my-24 h-full  flex gap-5 flex-col  '>
-            <h1 className='text-header font-migra mt-24 text-7xl  text-red-500'>about <span className='italic'>me</span></h1>
+            <AnimatedTitle text='about me' wordSpace={"mr-[14px]"}
+              charSpace={"mr-[0.001em]"} className=' font-migra mt-24 text-7xl text-red-500' />
+            <div ref={ref} className='text-3xl lg:text-5xl lg:w-10/12  '>
 
-            <div className='text-3xl lg:text-5xl lg:w-10/12  '>
-              Im an Indian front-end/full-stack and creative
+              <AnimatedBody delay={2} text=' Im an Indian front-end/full-stack and creative
               web developer keen eye for design and a knack for solving complex problems through simple and interactive interfaces.
-            </div>
+           '/>  </div>
             <div className='lg:text-xl text-lg lg:w-9/12'>
-              I am a Computer Science post-graduate, and curiosity dragged me to learn HTML &
+              <AnimatedBody delay={2.3} text='  I am a Computer Science post-graduate, and curiosity dragged me to learn HTML &
               CSS. Then, I learned Reactjs and TypeScript, among many other javascript frameworks then working on the MERN Stack.
+            '/>
             </div>
             <div className='lg:text-xl text-lg lg:w-9/12'>
-              My expertise in HTML, CSS, JavaScript, and React allows me to create dynamic and responsive web pages that engage users and deliver a seamless experience. Whether its crafting beautiful user interfaces, optimizing website performance, or developing complex web applications, I am always committed to delivering high-quality work that exceeds expectations.
-
+              <AnimatedBody delay={2.6} text=' My expertise in HTML, CSS, JavaScript, and React allows me to create dynamic and responsive web pages that engage users and deliver a seamless experience. Whether its crafting beautiful user interfaces, optimizing website performance, or developing complex web applications, I am always committed to delivering high-quality work that exceeds expectations.
+            '/>
             </div>
 
             <div className='lg:text-xl text-lg lg:w-9/12'>
-              In addition to my technical skills, I have a strong sense of creativity and an eye for detail that allows me to create visually stunning designs that are both functional and aesthetically pleasing. I believe that good design is the key to creating great user experiences, and I strive to incorporate this philosophy into everything I do.
-
+              <AnimatedBody delay={2.9} text=' In addition to my technical skills, I have a strong sense of creativity and an eye for detail that allows me to create visually stunning designs that are both functional and aesthetically pleasing. I believe that good design is the key to creating great user experiences, and I strive to incorporate this philosophy into everything I do.
+             '/>
             </div>
           </div>
         </div>
