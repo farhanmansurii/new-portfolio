84712a48b4e1cdde1f3646f79ac873a4016fa6f7
Farhan <mansurifarhanfm@gmail.com>
2023-05-03 06:17:03 +0530

    fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,11 +6,7 @@ export default function Navbar() {
   const router = useRouter()
 
   return (
-<<<<<<< HEAD
     <div className="fixed text-lg  text-blend-difference gap-2 mx-3 flex flex-row lg:flex-col  p-4 font-neue">
-=======
-    <div className="fixed text-lg gap-2 mx-3 flex flex-row lg:flex-col p-4 font-neue">
->>>>>>> 77e830f5cec556491abbe3b83423f75ec162da59
       <Link href='/'>
         <div className={`nav-link ${router.asPath === '/' ? 'text-red-500 mix-blend-difference' : 'text-black'}`}>farhan</div>
       </Link>
